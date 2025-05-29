61d0a073ec01aec93fcec86209cf413a0702e37f
Daniel G. <nuclearrocketcar@gmail.com>
2025-05-29 14:40:42 -0400

    Update ROPOTBEHAVROR_V3.ino

--- ROPOTBEHAVROR_V3.ino
+++ ROPOTBEHAVROR_V3.ino
@@ -34,10 +34,6 @@
 #include "Adafruit_Soundboard.h"
 
 
-
-//HEY YOU TURN THIS SHITSHOW INTO FUCKING FUNCTIONS!!!!!!!!!!!!!!!!!!
-
-
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
